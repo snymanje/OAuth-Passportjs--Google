e1057f4b8d25e7e83d04deeaf8e769a592cb9f2c
snymanje <jean.snyman@gmail.com>
2019-09-25 06:36:37 +0200

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const authRoutes = require("./routes/auth-routes");
+
+const app = express();
+
+//set up view engine
+app.set("view engine", "ejs");
+
+//setup routes
+app.use("/auth", authRoutes);
+
+//create home route
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("app now running on port 3000");
+});
